446a48ac65ae15ef2a9f6408393dc30f59fb939a
BiXiaopeng <bixiaopeng2007@gmail.com>
2015-03-16 23:37:21 +0900

    Initial Commit

---
+++ DateExtracter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DateExtracter
+//
+//  Created by BiXiaopeng on 15/3/16.
+//  Copyright (c) 2015年 BiXiaopeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DateExtracter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DateExtracter
+//
+//  Created by BiXiaopeng on 15/3/16.
+//  Copyright (c) 2015年 BiXiaopeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
